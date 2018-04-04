0ee74a0d91d4c5062d2f2328f468d88c84d14a19
Michael Glettig <michael.glettig@zuehlke.com>
2018-04-04 14:33:46 +0200

    Changed: For now still use old include path.

--- src/mgos_dns_sd.c
+++ src/mgos_dns_sd.c
@@ -36,7 +36,7 @@
 #include "mgos_ro_vars.h"
 #include "mgos_sys_config.h"
 #include "mgos_timers.h"
-#include "mongoose.h"
+#include "mongoose/mongoose.h"
 #ifdef MGOS_HAVE_WIFI
 #include "mgos_wifi.h"
 #endif
